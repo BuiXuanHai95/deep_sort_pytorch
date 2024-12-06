1b673f46684f99b7dd51d91a694b52e9d725fc21
BuiXuanHai95 <buixuanhai.mta@gmail.com>
2024-12-06 15:47:56 +0700

    kalman_filter.py

--- deep_sort/sort/kalman_filter.py
+++ deep_sort/sort/kalman_filter.py
@@ -227,3 +227,135 @@ class KalmanFilter(object):
             overwrite_b=True)
         squared_maha = np.sum(z * z, axis=0)
         return squared_maha
+
+
+class ExtendedKalmanFilter(object):
+    """
+    An Extended Kalman filter for tracking bounding boxes in image space.
+
+    The 8-dimensional state space
+
+        x, y, a, h, vx, vy, va, vh
+
+    contains the bounding box center position (x, y), aspect ratio a, height h,
+    and their respective velocities. Object motion follows a constant velocity model.
+    The bounding box location (x, y, a, h) is taken as a direct observation of the state space.
+    """
+
+    def __init__(self):
+        ndim, dt = 4, 1.0
+
+        # Create motion model (non-linear)
+        self._motion_mat = np.eye(2 * ndim, 2 * ndim)
+        for i in range(ndim):
+            self._motion_mat[i, ndim + i] = dt
+        self._update_mat = np.eye(ndim, 2 * ndim)
+
+        # Motion and observation uncertainty are chosen relative to the current state estimate.
+        self._std_weight_position = 1. / 20
+        self._std_weight_velocity = 1. / 160
+
+    def initiate(self, measurement):
+        """Create track from unassociated measurement."""
+        mean_pos = measurement
+        mean_vel = np.zeros_like(mean_pos)
+        mean = np.r_[mean_pos, mean_vel]
+
+        std = [
+            2 * self._std_weight_position * measurement[3],
+            2 * self._std_weight_position * measurement[3],
+            1e-2,
+            2 * self._std_weight_position * measurement[3],
+            10 * self._std_weight_velocity * measurement[3],
+            10 * self._std_weight_velocity * measurement[3],
+            1e-5,
+            10 * self._std_weight_velocity * measurement[3]]
+        covariance = np.diag(np.square(std))
+        return mean, covariance
+
+    def nonlinear_motion_model(self, mean):
+        """Nonlinear motion model."""
+        new_mean = np.copy(mean)
+        # Simulate constant velocity model for x, y, aspect ratio a, height h.
+        new_mean[0] += mean[4]  # x position update
+        new_mean[1] += mean[5]  # y position update
+        new_mean[2] += mean[6]  # aspect ratio update
+        new_mean[3] += mean[7]  # height update
+        return new_mean
+
+    def compute_motion_jacobian(self, mean):
+        """Compute the Jacobian of the motion model."""
+        jacobian = np.eye(len(mean))
+        jacobian[0, 4] = 1  # Derivative of x w.r.t vx
+        jacobian[1, 5] = 1  # Derivative of y w.r.t vy
+        jacobian[2, 6] = 1  # Derivative of a w.r.t va
+        jacobian[3, 7] = 1  # Derivative of h w.r.t vh
+        return jacobian
+
+    def project(self, mean, covariance):
+        """Project state distribution to measurement space."""
+        # We assume linear observation model: (x, y, a, h)
+        std = [
+            self._std_weight_position * mean[3],
+            self._std_weight_position * mean[3],
+            1e-1,
+            self._std_weight_position * mean[3]]
+        innovation_cov = np.diag(np.square(std))
+
+        mean = np.dot(self._update_mat, mean)
+        covariance = np.linalg.multi_dot((
+            self._update_mat, covariance, self._update_mat.T))
+        return mean, covariance + innovation_cov
+
+    def predict(self, mean, covariance):
+        """Run EKF prediction step."""
+        jacobian_motion = self.compute_motion_jacobian(mean)
+        mean = self.nonlinear_motion_model(mean)
+
+        std_pos = [
+            self._std_weight_position * mean[3],
+            self._std_weight_position * mean[3],
+            1e-2,
+            self._std_weight_position * mean[3]]
+        std_vel = [
+            self._std_weight_velocity * mean[3],
+            self._std_weight_velocity * mean[3],
+            1e-5,
+            self._std_weight_velocity * mean[3]]
+        motion_cov = np.diag(np.square(np.r_[std_pos, std_vel]))
+
+        covariance = np.linalg.multi_dot((
+            jacobian_motion, covariance, jacobian_motion.T)) + motion_cov
+
+        return mean, covariance
+
+    def update(self, mean, covariance, measurement):
+        """Run EKF correction step."""
+        projected_mean, projected_cov = self.project(mean, covariance)
+
+        chol_factor, lower = scipy.linalg.cho_factor(
+            projected_cov, lower=True, check_finite=False)
+        kalman_gain = scipy.linalg.cho_solve(
+            (chol_factor, lower), np.dot(covariance, self._update_mat.T).T,
+            check_finite=False).T
+        innovation = measurement - projected_mean
+
+        new_mean = mean + np.dot(innovation, kalman_gain.T)
+        new_covariance = covariance - np.linalg.multi_dot((
+            kalman_gain, projected_cov, kalman_gain.T))
+        return new_mean, new_covariance
+
+    def gating_distance(self, mean, covariance, measurements, only_position=False):
+        """Compute gating distance between state distribution and measurements."""
+        mean, covariance = self.project(mean, covariance)
+        if only_position:
+            mean, covariance = mean[:2], covariance[:2, :2]
+            measurements = measurements[:, :2]
+
+        cholesky_factor = np.linalg.cholesky(covariance)
+        d = measurements - mean
+        z = scipy.linalg.solve_triangular(
+            cholesky_factor, d.T, lower=True, check_finite=False,
+            overwrite_b=True)
+        squared_maha = np.sum(z * z, axis=0)
+        return squared_maha
